4a036fe346b41ed112c88f1e7c4b654081c55968
Your Name <kotvpaltoof@ya.ru>
2025-09-06 14:57:05 +0300

    purry forno

---
+++ commands/__init__.py
@@ -0,0 +1,5 @@
+
+
+def routers_pack() -> list:
+    return
+    return [main.router, exams.router, payment.router, profile.router, admin.router, review.router]
---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+
+from aiogram.types import BotCommand
+from commands import routers_pack
+
+from objects.bot import bot, dp
+import sys
+
+from datetime import datetime
+
+async def main():
+    print("=== === === BOT START === === ===")
+    print(f"python version: {sys.version.split()[0]}")
+    print(f"started at: {datetime.now()}")
+    for rt in routers_pack():
+        dp.include_router(rt)
+    
+    commands = [BotCommand(command="start", description='Перезагрузить бота')]
+    
+    botmy = await bot.get_me()
+    print(f"bot username: {botmy.username}")
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(commands=commands)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.ERROR)
+    asyncio.run(main())
