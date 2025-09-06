3828cefad15ddf79d61c573bf1cda6e4a09d9f47
Your Name <kotvpaltoof@ya.ru>
2025-09-06 17:06:53 +0300

    lil fix

--- utils/sferum.py
+++ utils/sferum.py
@@ -14,17 +14,23 @@ async def get_last_messages():
     print("gay sex")
     CFG = config.GET()
     DB = database.GET()
+    
+    return_data = []
+    
     for group in CFG.sferum_groups:
-        history = api.messages.get_history(peer_id=group, count=150, offset=0) 
+        print(group)
+        history = api.messages.get_history(peer_id=group, count=200, offset=0) 
         users_origin = history['response']['profiles']
-        pprint(users_origin)
         users = {uo["id"]: f"{uo['last_name']} {uo['first_name']}" for uo in users_origin}
         messages = history['response']['items']
         messages.reverse()
         last_message = DB[str(group)] if str(group) in DB.keys() else 0
         if messages[-1]["conversation_message_id"] > last_message:
             for_send = messages[last_message:]
-            pprint(for_send)
-            covered_msg = [{"msg_id": msg["conversation_message_id"], "text": msg["text"], "attachments": msg["attachments"], "author": users[msg["from_id"]]} for msg in for_send]
-            pprint(covered_msg)
-    return None
+            covered_msg = [{"msg_id": msg["conversation_message_id"], "text": msg["text"], "attachments": msg["attachments"], "author": users[msg["from_id"]]} for msg in for_send if msg["from_id"] > 0 ]
+            return_data.append({"group_id": group, "group_name": history["response"]["conversations"][0]["chat_settings"]["title"], "messages":covered_msg})
+            DB[str(group)] = messages[-1]["conversation_message_id"]
+            database.SAVE(DB)
+        else: pass
+        
+    pprint(return_data)
