044716b5d4ec6f916d7430278ea812ed34285ee7
Your Name <kotvpaltoof@ya.ru>
2025-09-06 16:36:33 +0300

    первичная обработка

--- utils/sferum.py
+++ utils/sferum.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import os
 from dotenv import load_dotenv
 
-from objects import config
+from objects import config, database
 
 load_dotenv()
 SFERUM_TOKEN = os.getenv("SFERUM_TOKEN")
@@ -13,9 +13,18 @@ api = ass.SferumAPI(remixdsid=SFERUM_TOKEN)
 async def get_last_messages():
     print("gay sex")
     CFG = config.GET()
+    DB = database.GET()
     for group in CFG.sferum_groups:
         history = api.messages.get_history(peer_id=group, count=150, offset=0) 
+        users_origin = history['response']['profiles']
+        pprint(users_origin)
+        users = {uo["id"]: f"{uo['last_name']} {uo['first_name']}" for uo in users_origin}
         messages = history['response']['items']
-        print(messages)
-    
+        messages.reverse()
+        last_message = DB[str(group)] if str(group) in DB.keys() else 0
+        if messages[-1]["conversation_message_id"] > last_message:
+            for_send = messages[last_message:]
+            pprint(for_send)
+            covered_msg = [{"msg_id": msg["conversation_message_id"], "text": msg["text"], "attachments": msg["attachments"], "author": users[msg["from_id"]]} for msg in for_send]
+            pprint(covered_msg)
     return None
