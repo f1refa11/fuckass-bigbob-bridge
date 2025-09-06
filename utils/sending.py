0c3156d748245d8adb3a8fa1721c0d70b40652cf
Your Name <kotvpaltoof@ya.ru>
2025-09-06 21:59:48 +0300

    немножко визуала

--- utils/sending.py
+++ utils/sending.py
@@ -5,19 +5,17 @@ async def sferum_messages(data: dict):
     print("gay anal")
     print(data)
     for iteral in data:
-        print("l")
         for msg in iteral["messages"]:
-            print("m")
             text = f'''
-Новое сообщение📩
+<b>Новое сообщение📩</b>
 Группа "{iteral['group_name']}" от {msg["author"]}
+<=>
 {msg['text']}
+<=>
 '''
             await send_message(text)
             
 async def send_message(text: str):
-    print('goofy ahh')
     CFG = config.GET()
-    print(CFG)
     await bot.bot.send_message(chat_id=CFG.tg_chat_id, text=text)
 
