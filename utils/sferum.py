1bfd5c743ba0ed2ae11ed17c3b898a36b52469bb
Your Name <kotvpaltoof@ya.ru>
2025-09-06 22:00:00 +0300

    багфикс

--- utils/sferum.py
+++ utils/sferum.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 
 from objects import config, database
+from utils import sending
 
 load_dotenv()
 SFERUM_TOKEN = os.getenv("SFERUM_TOKEN")
@@ -18,19 +19,26 @@ async def get_last_messages():
     return_data = []
     
     for group in CFG.sferum_groups:
-        print(group)
         history = api.messages.get_history(peer_id=group, count=200, offset=0) 
         users_origin = history['response']['profiles']
         users = {uo["id"]: f"{uo['last_name']} {uo['first_name']}" for uo in users_origin}
         messages = history['response']['items']
         messages.reverse()
-        last_message = DB[str(group)] if str(group) in DB.keys() else 0
-        if messages[-1]["conversation_message_id"] > last_message:
-            for_send = messages[last_message:]
-            covered_msg = [{"msg_id": msg["conversation_message_id"], "text": msg["text"], "attachments": msg["attachments"], "author": users[msg["from_id"]]} for msg in for_send if msg["from_id"] > 0 ]
-            return_data.append({"group_id": group, "group_name": history["response"]["conversations"][0]["chat_settings"]["title"], "messages":covered_msg})
-            DB[str(group)] = messages[-1]["conversation_message_id"]
-            database.SAVE(DB)
-        else: pass
-        
-    pprint(return_data)
+        sended_messages = DB[str(group)] if str(group) in DB.keys() else []
+        smsg_set = set(sended_messages)
+        filtered_msg = [msg for msg in messages if msg["conversation_message_id"] not in smsg_set and msg["from_id"] > 0]
+        if len(filtered_msg) == 0:
+            print("nothing")
+            pass
+        covered_messages = []
+        for msg in filtered_msg:
+            covered_messages.append({"msg_id": msg["conversation_message_id"], "text": msg["text"], "attachments": msg["attachments"], "author": users[msg["from_id"]]})
+            sended_messages.append(msg["conversation_message_id"])   
+        return_data.append({"group_id": group, "group_name": history["response"]["conversations"][0]["chat_settings"]["title"], "messages":covered_messages})
+        DB[str(group)] = sended_messages
+        database.SAVE(DB)
+
+    await sending.sferum_messages(return_data)
+    
+async def get_last_message_id():
+    return
