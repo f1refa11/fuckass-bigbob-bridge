d17974c8631024de6807865e91fad978c4f6b33a
f1refa11 <firekonstantin@mail.ru>
2025-09-07 12:25:00 +0300

    fix: add required imports

--- commands/main.py
+++ commands/main.py
@@ -1,3 +1,7 @@
+import logging
+
+import aiogram.filters
+import aiogram.types
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
@@ -9,10 +13,12 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_handler(msg: Message):
+    logging.info("Command: start")
     await msg.answer("go fuck yourself")
     await sferum.get_last_messages()
     
-@dp.message(Command("reset"))
-async def reser_queue(query: CallbackQuery):
+@dp.message(aiogram.filters.Command("reset"))
+async def reser_queue(query: aiogram.types.CallbackQuery):
+    logging.info("Command: reset")
     result = sferum.reset_queue()
     await query.answer(result)
